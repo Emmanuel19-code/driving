4c5d551ca1bf7b5aa20b705586cbcb23270ed522
unknown <emmanueladane@gmail.com>
2025-06-26 17:04:39 +0000

    update

--- src/app/dashboard/AlertCard.tsx
+++ src/app/dashboard/AlertCard.tsx
@@ -8,7 +8,7 @@ import { Rating } from "@mui/material";
 
 const AlertCard = () => {
   return (
-    <div className="bg-white shadow-md rounded pb-2  row-span-3 xl:row-span-6">
+    <div className="bg-white shadow-md rounded-lg pb-2  row-span-3 xl:row-span-6">
       <h3 className="text-lg font-semibold px-2 pt-2 pb-2">System Notifications</h3>
       <hr />
       <div className="overflow-auto h-56 p-1 scrollbar-hide">
